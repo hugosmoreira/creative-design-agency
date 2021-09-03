4941695f2462a5736b2948c0c6bb13917b0ad5b6
hugosmoreira <hugo.n4@gmail.com>
2021-09-02 17:33:52 -0700

    update header and navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
+import './App.css'
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Particles from 'react-particles-js';
 import Navbar from './components/Navbar';
-import './App.css'
+import Header from './components/Header';
+
 
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <Particles 
+       params={{
+         particles: {
+           number: {
+             value: 30,
+             density: {
+               enable: true,
+               value_area: 900
+             }
+           },
+           shape: {
+             type: "square",
+             stroke: {
+               width: 6,
+               color: "#0598ce"
+             }
+           }
+         }
+       }}
+      />
       <Navbar />
-    </div>
+      <Header />
+    </>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,34 +1,55 @@
-import React from 'react'
-import logo from '../assets/images/Devisionary.svg'
+import React from "react";
+import logo from "../assets/images/Devisionary.svg";
+// REACT FONTAWESOME IMPORTS
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-secondary">
-            <div className="container-fluid">
-            <a class="navbar-brand" href="#"><img className="logo" src={logo} alt="logo..." />  </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    
-                    <li class="nav-item">
-                        <a class="nav-link " href="#" tabindex="-1" aria-disabled="true">About Us</a>
-                    </li>
-                    
-                </ul>
-            </div>
-
-            </div>
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light ">
+        
             
-        </nav>
-    )
+        
+      <div className="container-fluid">
+        <div>
+            <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="logo..." /></a>
+        </div>
+        
+
+        <button class="navbar-toggler toggler-example" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1"
+        aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation"><span class="dark-blue-text"> <FontAwesomeIcon icon={faBars} style={{ color: "#fff" }} /></span></button>
+        
+        
+        <div class="collapse navbar-collapse" id="navbarSupportedContent1">
+            
+          <ul class="navbar-nav ms-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">about me</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">services</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">how work</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">portfolio</a>
+            </li>
+
+            <li className="nav-item">
+              <a className="nav-link" href="#">contacts</a>
+            </li>
+
+          </ul>
+
+        </div>
+
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
